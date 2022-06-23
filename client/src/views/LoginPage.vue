ee0e625214bd47200f7366109793ac7c59773868
banuprabowo <banuprabowo1996@gmail.com>
2022-06-23 23:35:00 +0700

    aplikasi belum disiapkan

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -14,52 +14,76 @@ export default {
     methods: {
         ...mapActions(useCounterStore, ["loginHandler"]),
 
-        checkLoginState() {
-            FB.getLoginStatus(function (response) {
-                statusChangeCallback(response);
-            });
-        }
+        // doc-facebook
+        // statusChangeCallback(response) {
+        //     console.log('statusChangeCallback');
+        //     console.log(response);
+            
+        //     // The response object is returned with a status field that lets the
+        //     // app know the current login status of the person.
+        //     // Full docs on the response object can be found in the documentation
+        //     // for FB.getLoginStatus().
+        //     if (response.status === 'connected') {
+        //         // Logged into your app and Facebook.
+        //         console.log('Welcome!  Fetching your information.... ');
+        //         FB.api('/me', function (response) {
+        //             console.log('Successful login for: ' + response.name);
+        //             document.getElementById('status').innerHTML =
+        //                 'Thanks for logging in, ' + response.name + '!';
+        //         });
+        //     } else {
+        //         // The person is not logged into your app or we are unable to tell.
+        //         document.getElementById('status').innerHTML = 'Please log ' +
+        //             'into this app.';
+        //     }
+        // },
+
+        // checkLoginState() {
+        //     FB.getLoginStatus(function (response) {
+        //         statusChangeCallback(response);
+        //     });
+        // },
 
         // firebase_x_fb
-        // facebookLogin() {
-        //     let that = this
-        //     const provider = new $nuxt.$fireModule.auth.FacebookAuthProvider()
-        //     this.$fire.auth.signInWithPopup(provider)
-        //         .catch(function (err) {
-        //             that.snackbarText = error.message
-        //             that.snackbar = true
-        //         }).then((user) => {
-        //             $nuxt.$router.push('/home')
-        //         })
-        // }
+        facebookLogin() {
+            let that = this
+            const provider = new $nuxt.$fireModule.auth.FacebookAuthProvider()
+            this.$fire.auth.signInWithPopup(provider)
+                .catch(function (err) {
+                    that.snackbarText = error.message
+                    that.snackbar = true
+                }).then((user) => {
+                    $nuxt.$router.push('/home')
+                })
+        }
 
     },
 
     created() {
         // doc_facebook
+        // this.checkLoginState()
+        // FB.getLoginStatus(function (response) {
+        //     statusChangeCallback(response);
+        // });
+        // window.fbAsyncInit = function () {
+        //     FB.init({
+        //         appId: '1382494195590177',
+        //         cookie: true,
+        //         xfbml: true,
+        //         version: 'v14.0'
+        //     });
 
-        FB.getLoginStatus(function (response) {
-            statusChangeCallback(response);
-        });
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '{your-app-id}',
-                cookie: true,
-                xfbml: true,
-                version: '{api-version}'
-            });
+        //     FB.AppEvents.logPageView();
 
-            FB.AppEvents.logPageView();
+        // };
 
-        };
-
-        (function (d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+        // (function (d, s, id) {
+        //     var js, fjs = d.getElementsByTagName(s)[0];
+        //     if (d.getElementById(id)) { return; }
+        //     js = d.createElement(s); js.id = id;
+        //     js.src = "https://connect.facebook.net/en_US/sdk.js";
+        //     fjs.parentNode.insertBefore(js, fjs);
+        // }(document, 'script', 'facebook-jssdk'));
     }
 }
 </script>
@@ -104,12 +128,20 @@ export default {
                             <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
                         </div>
 
-                        <!-- @click="facebookLogin"  -->
-                        <!-- <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with"
-                            data-layout="default" data-auto-logout-link="false" data-use-continue-as="false"></div> -->
+                        <button 
+                        @click="facebookLogin" 
+                        class="login-button my-2 pa-2"
+                        widht="100%"
+                        depressed
+                        large>
+                        <i class="bi bi-facebook"></i>Login with Facebook</button>
 
-                        <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-                        </fb:login-button>
+                        <!-- <div class="fb-login-button" data-width="" data-size="large"
+                            data-button-type="continue_with" data-layout="default" data-auto-logout-link="false"
+                            data-use-continue-as="false"></div> -->
+
+                        <!-- <fb:login-button id="status" scope="public_profile,email" onlogin="checkLoginState();">
+                        </fb:login-button> -->
 
                     </form>
                 </div>
